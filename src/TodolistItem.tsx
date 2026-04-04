cdf44afb8b92d6cc5b488f9e3aedf75797d08443
ElenaWebDev984 <badalova.dev.984@gmail.com>
2026-04-04 21:56:40 +0200

    add inputRef

--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -1,41 +1,69 @@
 import type {FilterValues, Task} from './App'
 import {Button} from './Button'
+import {useRef} from "react";
 
 type Props = {
-  title: string
-  tasks: Task[]
-  deleteTask: (taskId: string) => void
-  changeFilter: (filter: FilterValues) => void
+    title: string
+    tasks: Task[]
+    deleteTask: (taskId: string) => void
+    changeFilter: (filter: FilterValues) => void
+    addTask: (title: string) => void
 }
 
-export const TodolistItem = ({title, tasks, deleteTask, changeFilter}: Props) => {
-  return (
-      <div>
-        <h3>{title}</h3>
+export const TodolistItem = ({
+                                 title,
+                                 tasks,
+                                 deleteTask,
+                                 changeFilter,
+                                 addTask
+                             }: Props) => {
+
+    const inputRef = useRef<HTMLInputElement>(null)
+    console.log(inputRef.current)
+
+
+    return (
         <div>
-          <input/>
-          <Button title={'+'} />
+            <h3>{title}</h3>
+            <div>
+                <input ref={inputRef}/>
+                <Button title={'+'}
+                        onClick={() => {
+                            if (inputRef.current) {
+                                addTask(inputRef.current.value)
+                                inputRef.current.value = ''
+                            }
+                        }}
+                />
+            </div>
+            {tasks.length === 0 ? (
+                <p>Тасок нет</p>
+            ) : (
+                <ul>
+                    {tasks.map(task => {
+                        return (
+                            <li key={task.id}>
+                                <input type="checkbox" checked={task.isDone}/>
+                                <span>{task.title}</span>
+                                <Button title={'x'}
+                                        onClick={() => deleteTask(task.id)}
+                                />
+                            </li>
+                        )
+                    })}
+                </ul>
+            )}
+            <div>
+                <Button title={'All'}
+                        onClick={() => changeFilter('all')}
+                />
+                <Button title={'Active'}
+                        onClick={() => changeFilter('active')}
+                />
+                <Button title={'Completed'}
+                        onClick={() => changeFilter('completed')}
+                />
+            </div>
         </div>
-        {tasks.length === 0 ? (
-            <p>Тасок нет</p>
-        ) : (
-            <ul>
-              {tasks.map(task => {
-                return (
-                    <li key={task.id}>
-                      <input type="checkbox" checked={task.isDone}/>
-                      <span>{task.title}</span>
-                      <Button title={'x'} onClick={() => deleteTask(task.id)} />
-                    </li>
-                )
-              })}
-            </ul>
-        )}
-        <div>
-          <Button title={'All'} onClick={() => changeFilter('all')}/>
-          <Button title={'Active'} onClick={() => changeFilter('active')}/>
-          <Button title={'Completed'} onClick={() => changeFilter('completed')}/>
-        </div>
-      </div>
-  )
+    )
 }
